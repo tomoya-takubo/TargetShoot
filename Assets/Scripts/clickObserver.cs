fc8e07590099be2fb714ab24e91d2ced3f268b83
tomoya-takubo <tkb1003@gmail.com>
2020-03-27 20:33:34 +0900

    202003272032　初回コミット
    ～スコープがカーソルに追従、的並べたまで

---
+++ Assets/Scripts/clickObserver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickObserver : MonoBehaviour
+{
+
+    GameObject clickedGameObject;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+                //clickedGameObject.transform.localScale = new Vector2(0f, 0.6f);
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/targetBehavor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class targetBehavor : MonoBehaviour
+{
+    private float cnt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(0f, 0.6f, 0.6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(transform.localScale.x <= 0.6)
+        {
+            transform.localScale = new Vector3(cnt, 0.6f, 0.6f);
+        }
+        cnt += 0.1f;
+
+    }
+}
